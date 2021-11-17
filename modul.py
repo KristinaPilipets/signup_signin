c46effcf6e412cf04243369227d6f50fe86517e7
Kristina Pilipets <kris.tina.05@mail.ru>
2021-11-17 13:41:52 +0200

    функции

--- modul.py
+++ modul.py
@@ -1,26 +1,38 @@
 from random import*
-def sign_in (a:str,b:str):#авторизация
-    pass
-def psword_check(psword:str):
-    for i in range (0,13):
-        if psword[i].isdigit== True:
-            break
-def autopsword()->str:
+def psword_check(psword:str)->str:
+    """функция вернет True если пароль будет соответствовать всем параметрам
     """
+    for i in range(0,13,1): #так с каждой проверкой(заглавная буква, цифра и тд)
+        if psword[i].isdigit()== True:
+            digit="True"
+        if psword[i].isalpha()== True:
+            alpha="True"
+        if psword[i].isupper()== True:
+            upper="True"
+        if psword[i].islower()==True:
+            lower="True"
+        if psword[i] in [".",",","_"]:
+            symbl="True"
+    if digit=="True" and upper=="True" and alpha=="True" and lower=="True" and symbl=="True": #и тд со всеми вариантами
+        ans="True"
+    else:
+        ans="False"
+    return ans
+def autopsword()->str:
+    """auto created password
     """
     str0=".,:;!_*-+()/#¤%&"
-    str1 = '0123456789'
-    str2 = 'qwertyuiopasdfghjklzxcvbnm'
+    str1 = "0123456789"
+    str2 = "qwertyuiopasdfghjklzxcvbnm"
     str3 = str2.upper()
-    print(str3) # 'QWERTYUIOPASDFGHJKLZXCVBNM'
-    str4 = str0+str1+str2+str3
-    print(str4)
-    ls = list(str4)
-    print(ls)
-    random.shuffle(ls)
-    print(ls)
-    # Извлекаем из списка 12 произвольных значений
-    psword = ''.join([random.choice(ls) for x in range(12)])
+    #print(str3) # 'QWERTYUIOPASDFGHJKLZXCVBNM'
+    str4 = str0+str1+str2+str3 #сплошной текст со всеми str
+    # print(str4)
+    ls = list(str4) #создает список значений каждый символ через запятую
+    # print(ls)
+    shuffle(ls) #перемешиваем значения
+    # print(ls)
+    psword = "".join([choice(ls) for x in range(12)]) # Извлекаем из списка 12 произвольных значений
     # Пароль готов
-    print(psword)
+    # print(psword)
     return psword
--- signup_signin.py
+++ signup_signin.py
@@ -1,30 +1,44 @@
 from modul import*
 print("Регистрация и авторизация")
-s1=[]
-s2=[]
+users=["Kristina"]
+pswords=["qwerty"]
 while True:
     sign=input("Регистрация- R, Авторизация- A, Закончить- E")
     if sign.upper()=="R":
         log=input("Login: ")
         pswrd1=input("Хотитите ли вы использовать автоматичсеки созданный пароль? Y- да, N- нет")
-        if pswrd1.upper=="Y":
-            psword=autopsword
-        elif psword1.upper=="N":
+        if pswrd1.upper()=="Y":
+            psword=autopsword()
+            print(psword)
+            pswords.append(psword) #добавляем к списку 
+            if log not in users:
+                users.append(log)
+
+        elif pswrd1.upper()=="N":
             psword=""
             while len(psword)!=12:
                try:
-                   psword=input("Siseta parool: ")
+                   psword=input("Siseta parool 12 sümboolid suurus: ")
                except:
                     ValueError
-            ans=psword_check(a) #подходит ли пароль
-        #s1+log
-        #s2+psword
+            ans=psword_check(psword) #подходит ли пароль
+            if ans!= "True":
+                print ("Пароль не подходит")
+            else:
+                users+log
+                pswords+psword
+
     elif sign.upper()=="A":
         login=input("Siseta login: ")
-        psword=input("Siseta parool: ")
-        if login not in s1:
-            pass
-        if psword not in s2:
-            pass
-    else:
+        if login not in users:
+            print("несуществующий пользаватель")
+        else:
+            psword=input("Siseta parool: ")
+            if psword not in pswords:
+                print("Неправильный пароль")
+            else:
+                print("Успешнная авторизация")
+    elif sign.upper()=="E":
         break
+    else:
+        print("такой функции нету")
